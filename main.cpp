dc173e3825537d2e9207ad222b1c8069d76b2e87
xqill275 <oliverlong275@gmail.com>
2026-09-06 13:10:00 +0100

    clean up

--- main.cpp
+++ main.cpp
@@ -8,39 +8,41 @@
 // [!] take in only 2 (the program it self and the project name)
 // [!] create a folder in the current directory called the project name
 // [!] create a main.cpp in the new directory that is a simple hello world
-// [] create a CMakeLists.txt file
-// [] auto build the new proejct in a build directory
+// [!] create a CMakeLists.txt file
 const std::string VERSION_ = "060926";
 
-void createCMakeFile(const std::string &folderPath, const std::string projectName) {
-    std::string fileNane = folderPath + "/CMakeLists.txt";
-    std::cout << fileNane << "\n";
+void createCMakeFile(const std::string &folderPath, const std::string &projectName) {
+    std::string fileName = folderPath + "/CMakeLists.txt";
+    std::cout << fileName << "\n";
+
     std::ofstream file;
-    file.open(fileNane);
+    file.open(fileName);
     if (!file.is_open()) {
         std::cout << "Failed to create CMake file \n";
         return;
     }
+
     file << "cmake_minimum_required(VERSION 3.22)\n";
-    file << "project("<< projectName << ")\n";
+    file << "project(" << projectName << ")\n";
     file << "\n";
     file << "set (SOURCES\n";
     file << "   main.cpp\n";
     file << ")\n";
     file << "\n";
-    file << "add_executable("<<projectName<<" ${SOURCES})\n";
-    return;
+    file << "add_executable(" << projectName << " ${SOURCES})\n";
 }
 
 void createMain(const std::string &folderPath) {
-    std::string fileNane = folderPath + "/main.cpp";
-    std::cout << fileNane << "\n";
+    std::string fileName = folderPath + "/main.cpp";
+    std::cout << fileName << "\n";
+
     std::ofstream file;
-    file.open(fileNane);
+    file.open(fileName);
     if (!file.is_open()) {
         std::cout << "Failed to create main.cpp \n";
         return;
     }
+
     file << "#include <iostream>\n";
     file << "\n";
     file << "int main() {\n";
@@ -49,36 +51,32 @@ void createMain(const std::string &folderPath) {
 }
 
 std::optional<std::string> createDirectory(const std::string &folderName) {
-    // creates a folder with the passed name
-    // returns the folder path if succsessfull 
-    // return a Nullptr if failed;
-    std::string temp = "hello";
-    std::cout << folderName << "\n";
     bool folder = std::filesystem::create_directories(folderName);
     if (!folder) {
         return std::nullopt;
     }
-    std::filesystem::permissions(folderName, 
-                    std::filesystem::perms::owner_all | 
-                    std::filesystem::perms::group_all | 
+
+    std::filesystem::permissions(folderName,
+                    std::filesystem::perms::owner_all |
+                    std::filesystem::perms::group_all |
                     std::filesystem::perms::others_all,
                     std::filesystem::perm_options::replace);
-    class std::filesystem::__cxx11::path newDir = folderName;
-    class std::filesystem::__cxx11::path currentPath = std::filesystem::current_path();
-    currentPath /= newDir;
-    std::cout << currentPath << "\n";
-    return currentPath;
 
+    std::filesystem::path newDir = folderName;
+    std::filesystem::path currentPath = std::filesystem::current_path();
+    currentPath /= newDir;
+    return currentPath.string();
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 2 || argc > 2) {
+    if (argc != 2) {
         std::cout << "Usage:\n";
         std::cout << "    " << argv[0] << " <projectName> \n";
         exit(1);
     }
+
     std::string projectName = argv[1];
-    std::cout << projectName << "\n";
+
     if (std::optional<std::string> dirPath = createDirectory(projectName)) {
         createMain(*dirPath);
         createCMakeFile(*dirPath, projectName);
@@ -86,5 +84,6 @@ int main(int argc, char *argv[]) {
         std::cout << "Failed to make folder for " << projectName << " exiting...\n";
         exit(0);
     }
+
     return 0;
 }
